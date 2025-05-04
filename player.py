6b9feea2e299bf1890bbc5a9fd3c7949dfd05a55
shay-dr <shay.davidr@gmail.com>
2025-05-04 13:27:21 -0600

    added ability for the player to "move" forward or backward.

--- player.py
+++ player.py
@@ -22,15 +22,27 @@ class Player(circleshape.CircleShape):
     def rotate(self,dt):
         self.rotation += constants.PLAYER_TURN_SPEED * dt
         
-    def update(self, dt):
+    def update(self,dt):
         keys = pygame.key.get_pressed()
 
         #rotate Left
         if keys[pygame.K_a] or keys[pygame.K_LEFT]:
             self.rotate(-dt)
 
-
         #rotate right
         if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
             self.rotate(dt)
+                #rotate Left
 
+        #move forwared        
+        if keys[pygame.K_w] or keys[pygame.K_UP]:
+            self.move(dt)
+
+        #move backward
+        if keys[pygame.K_s] or keys[pygame.K_DOWN]:
+            self.move(-dt)
+
+
+    def move(self,dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * constants.PLAYER_SPEED * dt
